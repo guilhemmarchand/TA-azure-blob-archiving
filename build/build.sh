c88197a25a13e861e6aa1ad18195bed32ebe40a3
Guilhem Marchand <guilhem.marchand@gmail.com>
2022-01-17 14:32:06 +0000

    minor fix

--- build/build.sh
+++ build/build.sh
@@ -9,7 +9,7 @@ OUTPUT="../output"
 app="TA-azure-blob-archiving"
 version=$(grep 'version =' ../$app/default/app.conf | head -1 | awk '{print $3}' | sed 's/\.//g')
 
-rm -f *.tgz ${app}
+rm -rf *.tgz ${app}
 cp -a ../${app} .
 tar -czf ../output/${app}_${version}.tgz --exclude=${app}/local --exclude=${app}/metadata/local.meta ${app}
 echo "Wrote: ${app}_${version}.tgz"
