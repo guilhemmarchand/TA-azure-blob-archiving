7c5127c3468efd6be7c37be5348d26119539e0ce
Guilhem Marchand <guilhem.marchand@gmail.com>
2022-01-18 12:04:59 +0000

    migration to ucc-gen

--- build/build.sh
+++ build/build.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash
-# set -x
+#set -x
 
 # for Mac OS X
 export COPYFILE_DISABLE=true
 
 PWD=$(pwd)
-OUTPUT="../output"
-app="TA-azure-blob-archiving"
-version=$(grep 'version =' ../$app/default/app.conf | head -1 | awk '{print $3}' | sed 's/\.//g')
+OUTDIR="output"
 
-rm -rf *.tgz ${app}
-cp -a ../${app} .
-tar -czf ../output/${app}_${version}.tgz --exclude=${app}/local --exclude=${app}/metadata/local.meta ${app}
+app="TA-azure-blob-archiving"
+version=$(grep 'version =' ../version.txt | head -1 | awk '{print $3}' | sed 's/\.//g')
+ta_version=$(grep 'version =' ../version.txt | head -1 | awk '{print $3}')
+
+cd ../
+ucc-gen --ta-version "$ta_version"
+
+cd "${OUTDIR}"
+find . -name "*.pyc" -type f -exec rm -f {} \;
+rm -f *.tgz
+if [ -f ${app}/metadata/local.meta ]; then
+    rm -f ${app}/metadata/local.meta
+fi
+tar -czf ${app}_${version}.tgz ${app}
 echo "Wrote: ${app}_${version}.tgz"
+
+sha256=$(sha256sum ${app}_${version}.tgz)
+echo "Wrote: ${sha256} in $OUTDIR"
+echo ${sha256} >release-sha256.txt
 
 exit 0
