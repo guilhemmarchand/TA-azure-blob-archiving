363712bdc866fe388bdd76c1bb4c29decbb489b5
Guilhem Marchand <guilhem.marchand@gmail.com>
2024-04-09 19:21:26 +0100

    fix readthedocs issue

--- docs/conf.py
+++ docs/conf.py
@@ -63,7 +63,7 @@ release = u'1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
@@ -194,7 +194,7 @@ html_static_path = ['.static']
 # Sphinx supports the following languages:
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
+html_search_language = 'en'
 
 # A dictionary with options for the search language support, empty by default.
 # Now only 'ja' uses this config value
